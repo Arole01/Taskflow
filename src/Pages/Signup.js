609dd737b285dfd594302d2ef6c15f0bae1b8e9e
Arole01 <ajosedavidayobami@gmail.com>
2025-10-17 23:21:02 +0100

    signupComplete

--- src/Pages/Signup.js
+++ src/Pages/Signup.js
@@ -31,7 +31,7 @@ const Signup = () => {
     const [cities,setCities] = useState([])
     const [selectedCountry,setSelectedCountry] = useState("")
     const [loadingCities,setLoadingCities] = useState(false)
-    const {handleSubmit,register, formState:{errors, isSubmitSuccessful}, reset,} = useForm({resolver:yupResolver(schema)})
+    const {handleSubmit,register, formState:{errors, isSubmitSuccessful}, setValue, reset,} = useForm({resolver:yupResolver(schema)})
 
     useEffect(() => {
         const fetchCountries = async () => {
@@ -85,7 +85,87 @@ const Signup = () => {
         <div>
             <form onSubmit={handleSubmit(onSubmit)}>
                 <h2>Create account</h2>
+                <label>First Name</label>
+            <input type="text" placeholder="First name" {...register("firstname")}/>
+            {errors.name && <p style={{color:"red"}}>{errors.name.message}</p>}
+            <label>Last Name</label>
+            <input type="text" placeholder="Last name" {...register("lastname")}/>
+            {errors.name && <p style={{color:"red"}}>{errors.name.message}</p>}
+            <label>Email</label>
+            <input type="email" placeholder="Enter your email" {...register("email")}/>
+            {errors.email && <p style={{color:"red"}}>{errors.email.message}</p>}
+            <label>Phone number</label>
+            <input type="tel" {...register("phone")} placeholder="Enter your phone number"/>
+            {errors.phone && <p style={{color:"red"}}>
+            {errors.phone.message}</p>}
+            <label>Password</label>
+            <input type={showPassword ? "text" : "password"} placeholder="Enter your password" {...register("password")}/>
+            {errors.password && <p style={{color:"red"}}>{errors.password.message}</p>}
+            <label>Confirm Password</label>
+            <input type={showPassword ? "text" : "password"} placeholder="Confirm your password" {...register("confirmPassword")}/>
+            {errors.confirmPassword && <p style={{color:"red"}}>{errors.confirmPassword.message}</p>}
+
+            <div className="checkbox-container">
+                    <input 
+                        type="checkbox"
+                        id="showPassword"
+                        checked={showPassword}  
+                        onChange={(e) => setShowPassword(e.target.checked)}
+                    />
+                    <label htmlFor="showPassword">Show Password</label> 
+                </div>
+
+                <label>Gender</label>
+                <select {...register("gender")}>
+                    <option value="">Select Gender</option>
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                </select>
+                {errors.gender && <p style={{color:"red"}}>{errors.gender.message}</p>}
+
+                <label>Date of Birth</label>
+                <input type="date" {...register("dob")}/>
+                {errors.dob && <p style={{color:"red"}}>{errors.dob.message}</p>}
+
+
+                <label>Address</label>
+                <input type="text" {...register("address")} placeholder="Enter your address"/>
+                {errors.address && <p style={{color:"red"}}>{errors.address.message}</p>}
+
+                <label>Country</label>
+                <select {...register("country")}
+                onChange={(e) => {
+                    setSelectedCountry(e.target.value);
+                    setValue("country", e.target.value);
+                    setValue("city", "")
+                }}>
+                <option value="">Select Country</option>
+                {countries.map((country, index) => (
+                    <option key={index} value={country}>
+                        {country}
+                    </option>
+                ))}
+                </select>
+                {errors.country && <p style={{color:"red"}}>{errors.country.message}</p>}
+
+                <label>City</label>
+                <select {...register("city")}
+                disabled={!selectedCountry || loadingCities}>
+                    <option value="">
+                        {loadingCities ? "Loading cities..." : "Select City"}
+                    </option>
+                    {cities.map((city, index) => (
+                        <option key={index} value={city}>
+                            {city}
+                        </option>
+                    ))}
+                </select>
+                {errors.city && <p style={{color:"red"}}>{errors.city.message}</p>}
+            
+            <button className="bttn" disabled={loading}>{loading? "Signing up": "Sign up"}</button>
             </form>
         </div>
     )
 }
+
+export default Signup
