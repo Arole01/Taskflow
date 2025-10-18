58130eff714e6cebb50ad125197832914ce6a5e1
Arole01 <ajosedavidayobami@gmail.com>
2025-10-18 21:15:07 +0100

    inputfield

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -9,4 +9,39 @@ const Home = () => {
         setTasks([...tasks, { text: newTask, completed:false}]);
         setNewTask("");
     };
+
+    const toggleComplete = (index) => {
+        const updated = tasks.map((task,i) =>
+        i === index ? { ...task, completed: !task.completed } : task
+        )
+        setTasks(updated);
+    }
+
+    const deleteTask = (index) => {
+        setTasks(tasks.filter((_, i) => i !== index));
+    };
+
+    return (
+        <div className="home">
+            <h1>TaskFlow Dashboard</h1>
+
+            <input type="text"
+                    value={newTask}
+                    placeholder="Enter a new task"
+                    onChange={(e) => setNewTask(e.target.value)}/>
+
+                    <button onClick={addTask}>Add Task</button>
+
+                    <ul>
+                        {tasks.map((task, i) => (
+                            <li key={i}>
+                                <span onClick={() => toggleComplete(i)}>
+                                    {task.text}
+                                </span>
+                                <button onClick={() => deleteTask(i)}>❌</button>
+                            </li>
+                        ))}
+                    </ul>
+        </div>
+    )
 }
